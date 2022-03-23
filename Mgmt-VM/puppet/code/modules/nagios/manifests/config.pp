d8a83e986ccc92309a7e52d0207cd9369afc8651
Anthony Legg <leggtc1@student.op.ac.nz>
2022-03-23 05:24:14 +0000

    mgmt: automated system backup
    completed: 23-03-2022 05:24:14 UTC
    
    mgmt-b

--- Mgmt-VM/puppet/code/modules/nagios/manifests/config.pp
+++ Mgmt-VM/puppet/code/modules/nagios/manifests/config.pp
@@ -22,6 +22,7 @@ class nagios::config {
 	group => 'puppet',
 	}
 
+
 	nagios_host { 'db-b.foo.org.nz':
 	target => '/etc/nagios3/conf.d/ppt_hosts.cfg',
 	alias => 'db',
@@ -31,6 +32,60 @@ class nagios::config {
 	notification_interval => 30,
 	notification_period => '24x7',
 	notification_options => 'd,u,r',
-	mode => '0644',
-}	
-}  
+	mode => '0444',
+	}	
+
+	nagios_host { 'app-b.foo.org.nz':
+        target => '/etc/nagios3/conf.d/ppt_hosts.cfg',
+        alias => 'app',
+        check_period => '24x7',
+        max_check_attempts => 3,
+        check_command => 'check-host-alive',
+        notification_interval => 30,
+        notification_period => '24x7',
+        notification_options => 'd,u,r',
+        mode => '0444',
+	}
+	nagios_host { 'backup-b.foo.org.nz':
+        target => '/etc/nagios3/conf.d/ppt_hosts.cfg',
+        alias => 'backup',
+        check_period => '24x7',
+        max_check_attempts => 3,
+        check_command => 'check-host-alive',
+        notification_interval => 30,
+        notification_period => '24x7',
+        notification_options => 'd,u,r',
+        mode => '0444',
+
+	}
+
+	nagios_host { 'mgmt-b.foo.org.nz':
+        target => '/etc/nagios3/conf.d/ppt_hosts.cfg',
+        alias => 'mgmt',
+        check_period => '24x7',
+        max_check_attempts => 3,
+        check_command => 'check-host-alive',
+        notification_interval => 30,
+        notification_period => '24x7',
+        notification_options => 'd,u,r',
+        mode => '0444',
+
+	}
+
+ 
+	nagios_hostgroup {"my-ssh-servers":
+	target => "/etc/nagios3/conf.d/ppt_hostgroups.cfg",
+	mode => "0444",
+	alias => 'My SSH servers',
+	members => 'backup-b.foo.org.nz, mgmt-b.foo.org.nz, db-b.foo.org.nz, app-b.foo.org.nz'
+	}
+
+	nagios_hostgroup {"my-db-servers":
+        target => "/etc/nagios3/conf.d/ppt_hostgroups.cfg",
+        mode => "0444",
+        alias => 'My DB servers',
+        members => 'db-b.foo.org.nz'
+        }
+
+	
+}
