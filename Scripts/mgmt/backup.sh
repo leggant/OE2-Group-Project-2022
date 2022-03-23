c93947b46d34ed4608572b385b2682d98222ce41
Anthony Legg <leggtc1@student.op.ac.nz>
2022-03-23 18:21:20 +1300

    mgmt: added backup script to run on a crontab
    mgmt-b

---
+++ Scripts/mgmt/backup.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+COMMIT_TIMESTAMP=`date +'%Y-%m-%d %H:%M:%S %Z'`
+cd ~/OE2-Group-Project
+git pull
+sudo cp /etc/puppet/ -r ~/OE2-Group-Project/Mgmt-VM/ -r
+sudo rm -r ~/OE2-Group-Project/Mgmt-VM/puppet/ssl
+sudo cp /etc/puppet/code/environments/production/manifests/site.pp ~/OE2-Group-Project/Mgmt-VM/puppet/
+sudo chown bitstudent ~/OE2-Group-Project/Mgmt-VM/puppet/code
+sudo chgrp bitstudent ~/OE2-Group-Project/Mgmt-VM/puppet
+sudo chown bitstudent ~/OE2-Group-Project/Mgmt-VM/puppet/* 
+sudo chgrp bitstudent ~/OE2-Group-Project/Mgmt-VM/puppet/*
+sudo cp /etc/hosts ~/OE2-Group-Project/Mgmt-VM/hosts
+sudo chown bitstudent ~/OE2-Group-Project/Mgmt-VM/hosts
+sudo chgrp bitstudent ~/OE2-Group-Project/Mgmt-VM/hosts
+sudo chmod go+r ~/OE2-Group-Project/Mgmt-VM/hosts
+sudo cp /etc/nagios3 -r ~/OE2-Group-Project/Mgmt-VM/ -r 
+sudo chown bitstudent ~/OE2-Group-Project/Mgmt-VM/nagios3 
+sudo chgrp bitstudent ~/OE2-Group-Project/Mgmt-VM/nagios3 
+sudo chown bitstudent ~/OE2-Group-Project/Mgmt-VM/nagios3/* 
+sudo chgrp bitstudent ~/OE2-Group-Project/Mgmt-VM/nagios3/* 
+sudo cp /etc/php/7.2/cgi/php.ini ~/OE2-Group-Project/Mgmt-VM/php.ini 
+sudo chown bitstudent ~/OE2-Group-Project/Mgmt-VM/php.ini 
+sudo chgrp bitstudent ~/OE2-Group-Project/Mgmt-VM/php.ini 
+history -w 
+sudo cp ~/.bash_history ~/OE2-Group-Project/Mgmt-VM/bash_history.txt
+sudo chown bitstudent ~/OE2-Group-Project/Mgmt-VM/bash_history.txt 
+sudo chgrp bitstudent ~/OE2-Group-Project/Mgmt-VM/bash_history.txt 
+sudo chmod go+r ~/OE2-Group-Project/Mgmt-VM/bash_history.txt 
+sudo cp ~/.bashrc ~/OE2-Group-Project/Mgmt-VM/bashrc.txt 
+sudo chown bitstudent ~/OE2-Group-Project/Mgmt-VM/bashrc.txt 
+sudo chgrp bitstudent ~/OE2-Group-Project/Mgmt-VM/bashrc.txt 
+sudo chmod go+r ~/OE2-Group-Project/Mgmt-VM/bashrc.txt 
+cd ~/OE2-Group-Project 
+git status
+git add .
+git commit -m "mgmt: automated system backup" -m "completed: $COMMIT_TIMESTAMP" -m "mgmt-b"
