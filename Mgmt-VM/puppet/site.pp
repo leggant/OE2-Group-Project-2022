f90293f073c181566dcba9aa5d65e56307e965b6
Anthony Legg <leggtc1@student.op.ac.nz>
2022-03-31 08:00:02 +0000

    mgmt: automated system backup
    completed: 31-03-2022 08:00:02 UTC
    
    mgmt-b

--- Mgmt-VM/puppet/site.pp
+++ Mgmt-VM/puppet/site.pp
@@ -2,16 +2,19 @@ node 'db-b.foo.org.nz' {
     include sudo
     include ntp_service
     include mariadb
+    service { 'puppet': ensure => 'running', enable => true }
     package { 'vim': ensure => present }
 }
 node 'backup-b.foo.org.nz' {
     include sudo
     include ntp_service
+    service { 'puppet': ensure => 'running', enable => true }
     package { 'vim': ensure => present }
 }
 node 'app-b.foo.org.nz' {
     include sudo
     include ntp_service
+    service { 'puppet': ensure => 'running', enable => true }
     package { 'vim': ensure => present }
 }
 node 'mgmt-b.foo.org.nz' {
