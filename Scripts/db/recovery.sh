f6886d938517cc7ca7673353e0dd7ef4a58731ff
Anthony Legg <leggtc1@student.op.ac.nz>
2022-04-13 05:30:15 +0000

    db: add owncloud recovery script
    db-b

--- Scripts/db/recovery.sh
+++ Scripts/db/recovery.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+
+
+
 sudo apt update
 sudo apt install mysql-client
-sudo mysql --user=root --password=P@ssw0rd -e "create database if not exists testcloud; grant all on testcloud.* to 'testcloud'@'app-b.foo.org.nz' identified by 'P@ssw0rd';flush privileges;exit;"
+sudo mysql --user=root --password=P@ssw0rd -e "create database if not exists testcloud;" 
+sudo mysql -e "grant all on testcloud.* to 'testcloud'@'app-b.foo.org.nz' identified by 'P@ssw0rd';" 
+sudo mysql -e "flush privileges;" 
+
 
 # check for file changes
 
