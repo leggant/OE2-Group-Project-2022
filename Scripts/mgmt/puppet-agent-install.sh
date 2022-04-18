921105a0bac1217e3330be98f85ea475bf8429df
Anthony Legg <leggtc1@student.op.ac.nz>
2022-04-18 04:07:02 +0000

    mgmt: automated system backup
    completed: 18-04-2022 04:07:02 UTC
    
    mgmt-b

---
+++ Scripts/mgmt/puppet-agent-install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo apt-get install puppet
+
+LINE=[agent]
+LINE1=server=mgmt-b.foo.org.nz
+FILE=/etc/puppet/puppet.conf
+
+if
+grep -qF "$LINE" "$FILE"; then
+echo "already exists"
+else
+echo $LINE >> $FILE
+echo "done"
+fi
+
+if
+grep -qF "$LINE1" "$FILE"; then
+echo "already exists"
+else
+sudo sed -i "/^\[agent]/a $LINE1" "$FILE"
+echo "done"
+fi
+
+sudo puppet agent --server=mgmt-b.foo.org.nz --no-daemonize --verbose --onetime
+
+sudo systemctl status puppet
---
+++ Scripts/mgmt/puppet-master-install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo apt-get install puppetmaster -y
+
+
+
+LINE=certname=mgmt-b.foo.org.nz
+LINE2=server=mgmt-b.foo.org.nz
+FILE=/etc/puppet/puppet.conf
+
+if
+grep -qF "$LINE" "$FILE"; then
+echo "already exists"
+else
+sudo sed -i "/^\[master]/a $LINE" "$FILE"
+echo "done"
+fi
+
+
+if
+grep -qF "$LINE2" "$FILE"; then
+echo "already exists"
+else
+sudo sed -i "/^\[main]/a $LINE2" "$FILE"
+echo "done"
+fi
+
+dir=/etc/puppet/code/environments/production/manifests/
+mkdir -p $dir && touch $dir/site.pp
+
+sudo systemctl restart puppetmaster
+
+
+
+
