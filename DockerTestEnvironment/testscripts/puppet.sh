d2033f5ddbf9aaa6a3403a59f479f62b586aa39d
Anthony Legg <leggtc1@student.op.ac.nz>
2022-04-24 00:50:00 +1200

    scripts: updated docker test container
    puppetmaster script is now working, puppet script still failing

--- DockerTestEnvironment/testscripts/puppet.sh
+++ DockerTestEnvironment/testscripts/puppet.sh
@@ -1,12 +1,8 @@
-#!/bin/sh
-
-apt-get update
-apt-get install puppet -y
-
-LINE=[agent]
+#!/bin/bash
+apt-get install puppet
+LINE='[agent]'
 LINE1=server=mgmt-b.foo.org.nz
 FILE=/etc/puppet/puppet.conf
-
 if
 grep -qF "$LINE" "$FILE"; then
 echo "already exists"
@@ -14,17 +10,13 @@ else
 echo $LINE >> $FILE
 echo "done"
 fi
-
-if
-grep -qF "$LINE1" "$FILE"; then
-echo "already exists"
-else
-sed -i "/^\[agent]/a $LINE1" "$FILE"
-echo "done"
-fi
-
+# if
+# grep -qF "$LINE1" "$FILE"; then
+# echo "already exists"
+# else
+# sed -i "/^\[agent]/a $LINE1" "$FILE"
+# echo "done"
+# fi
 puppet agent --server=mgmt-b.foo.org.nz --no-daemonize --verbose --onetime
-
-#sudo systemctl status puppet
-service puppet start
-service puppet status
+# service puppet start
+# service puppet status
