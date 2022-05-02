5370bbead9fa40ea21ad39917eadc0c1a580235a
Anthony Legg <leggtc1@student.op.ac.nz>
2022-05-02 11:28:02 +0000

    mgmt: automated system backup
    completed: 02-05-2022 11:28:02 UTC
    
    mgmt-b

--- Mgmt-VM/puppet/code/modules/php/manifests/config.pp
+++ Mgmt-VM/puppet/code/modules/php/manifests/config.pp
@@ -3,8 +3,4 @@ class php::config {
 		command => 'sudo add-apt-repository ppa:ondrej/php -y',
 		provider => shell,
 	}
-	exec { 'update':
-		command => 'sudo apt-get update',
-                provider => shell,
-	}
 }
