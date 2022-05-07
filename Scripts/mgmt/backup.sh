f30831298df507c06ac136b9a2b949323a107b52
Anthony Legg <leggtc1@student.op.ac.nz>
2022-05-07 02:49:54 +0000

    mgmt: automated system backup
    completed: 07-05-2022 02:49:53 UTC
    
    mgmt-b

--- Scripts/mgmt/backup.sh
+++ Scripts/mgmt/backup.sh
@@ -23,36 +23,18 @@ cd ~/
 sudo cp /etc/puppet/ -r ~/OE2-Group-Project/Mgmt-VM/ -r
 sudo rm -r ~/OE2-Group-Project/Mgmt-VM/puppet/ssl
 sudo cp /etc/puppet/code/environments/production/manifests/site.pp ~/OE2-Group-Project/Mgmt-VM/puppet/
-sudo chown bitstudent ~/OE2-Group-Project/Mgmt-VM/puppet/code
-sudo chgrp bitstudent ~/OE2-Group-Project/Mgmt-VM/puppet
-sudo chown bitstudent ~/OE2-Group-Project/Mgmt-VM/puppet/*
-sudo chgrp bitstudent ~/OE2-Group-Project/Mgmt-VM/puppet/*
 sudo cp /etc/hosts ~/OE2-Group-Project/Mgmt-VM/hosts
-sudo chown bitstudent ~/OE2-Group-Project/Mgmt-VM/hosts
-sudo chgrp bitstudent ~/OE2-Group-Project/Mgmt-VM/hosts
 sudo chmod go+r ~/OE2-Group-Project/Mgmt-VM/hosts
 sudo cp /etc/nagios3 -r ~/OE2-Group-Project/Mgmt-VM/ -r
-sudo chown bitstudent ~/OE2-Group-Project/Mgmt-VM/nagios3
-sudo chgrp bitstudent ~/OE2-Group-Project/Mgmt-VM/nagios3
-sudo chown bitstudent ~/OE2-Group-Project/Mgmt-VM/nagios3/*
-sudo chgrp bitstudent ~/OE2-Group-Project/Mgmt-VM/nagios3/*
 sudo cp /etc/php/7.2/cgi/php.ini ~/OE2-Group-Project/Mgmt-VM/php.ini
-sudo chown bitstudent ~/OE2-Group-Project/Mgmt-VM/php.ini
-sudo chgrp bitstudent ~/OE2-Group-Project/Mgmt-VM/php.ini 
 sudo cp ~/.bash_history ~/OE2-Group-Project/Mgmt-VM/bash_history.txt
-sudo chown bitstudent ~/OE2-Group-Project/Mgmt-VM/bash_history.txt
-sudo chgrp bitstudent ~/OE2-Group-Project/Mgmt-VM/bash_history.txt
 sudo chmod go+r ~/OE2-Group-Project/Mgmt-VM/bash_history.txt
 sudo cp ~/.bashrc ~/OE2-Group-Project/Mgmt-VM/bashrc
-sudo chown bitstudent ~/OE2-Group-Project/Mgmt-VM/bashrc
-sudo chgrp bitstudent ~/OE2-Group-Project/Mgmt-VM/bashrc
 sudo chmod go+r ~/OE2-Group-Project/Mgmt-VM/bashrc
 sudo cp /etc/nagios ~/OE2-Group-Project/Mgmt-VM -r
 sudo cp /etc/nagios-plugins ~/OE2-Group-Project/Mgmt-VM -r
 sudo cp /etc/node-exporter ~/OE2-Group-Project/Mgmt-VM/node-exporter -r
 sudo cp /etc/systemd/system/node-exporter.service ~/OE2-Group-Project/Mgmt-VM/node-exporter/node-exporter.service
-sudo chown -R bitstudent ~/OE2-Group-Project/Scripts
-sudo chgrp -R bitstudent ~/OE2-Group-Project/Scripts
 sudo chown -R bitstudent ~/OE2-Group-Project/Mgmt-VM
 sudo chgrp -R bitstudent ~/OE2-Group-Project/Mgmt-VM
 sudo chmod -R 775 ~/OE2-Group-Project/Scripts
