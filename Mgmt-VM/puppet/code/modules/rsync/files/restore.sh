2d6a8dece71b2e8e341edc706b2de011a11b8d18
Anthony Legg <leggtc1@student.op.ac.nz>
2022-05-09 11:00:02 +0000

    mgmt: automated system backup
    completed: 09-05-2022 11:00:02 UTC
    
    mgmt-b

--- Mgmt-VM/puppet/code/modules/rsync/files/restore.sh
+++ Mgmt-VM/puppet/code/modules/rsync/files/restore.sh
@@ -10,7 +10,13 @@ host=$(hostname)
 if [ $host == $mgmt ]
 then
 
-BDIR="/home/$USER/test/ /home/$USER/s/"
+BDIR="
+/etc/node-exporter
+/etc/nagios
+/etc/nagios3
+/etc/nagios-plugins
+/etc/puppet
+"
 
 # excludes file - this contains a wildcard pattern per line of files to exclude
 EXCLUDES="$HOME/cron/excludes"
@@ -19,12 +25,22 @@ INCLUDE="$HOME/test/*.txt"
 BSERVER=groupb
 USERX=restore-b.foo.org.nz
 BACKUPDIR=`date +%d-%m-%Y-%H-%M-%S`
-OPTS=" -v" 
+OPTS="-haAXuv -v --exclude --log-file=/home/$USER/log.log --backup-dir=~/backup/mgmt/backup-$BACKUPDIR" 
 
 for d in $BDIR;do
 rsync $OPTS $d $BSERVER@$USERX:~/backup/mgmt
 done
 
-else
-echo " another vm"
+
+
+elif[$host == $db]
+then
+echo " db vm"
+elif[$host == $app]
+then
+echo " app vm"
+else[$host == $backup]
+
+echo " backup vm"
+
 fi
