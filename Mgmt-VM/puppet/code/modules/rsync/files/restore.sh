bbd22a178b24d879f258dc13879f499ec17366f6
Anthony Legg <leggtc1@student.op.ac.nz>
2022-05-11 08:30:02 +0000

    mgmt: automated system backup
    completed: 11-05-2022 08:30:02 UTC
    
    mgmt-b

--- Mgmt-VM/puppet/code/modules/rsync/files/restore.sh
+++ Mgmt-VM/puppet/code/modules/rsync/files/restore.sh
@@ -12,7 +12,7 @@ then
 SUFFIX=$(date +%j)
 remote_backup=restore-b.foo.org.nz
 #ssh groupb@$remote_backup ls Backups/my_work/backup_'$SUFFIX' && rm -r Backups/my_work/backup_'$SUFFIX'
-OPTS="-ab --recursive --files-from='to_backup.txt' --backup-dir='backup_$SUFFIX' --delete --filter 'protect backup_*'"
+OPTS="-ab --recursive --files-from='to_backup.txt' --backup-dir='backup_$SUFFIX' --delete "
 rsync $OPTS /etc/puppet/code/modules/rsync/files/to_backup.txt groupb@$remote_backup:~/Backups/my_work
 elif [ $host == $db]	
 then
