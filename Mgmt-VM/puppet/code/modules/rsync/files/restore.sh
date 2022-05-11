f093070bc591ea4244d42976fe557dbc6dbbb149
Anthony Legg <leggtc1@student.op.ac.nz>
2022-05-11 05:13:58 +0000

    mgmt: automated system backup
    completed: 11-05-2022 05:13:57 UTC
    
    mgmt-b

--- Mgmt-VM/puppet/code/modules/rsync/files/restore.sh
+++ Mgmt-VM/puppet/code/modules/rsync/files/restore.sh
@@ -34,7 +34,7 @@ done
 
 
 #=========================================================================
-elif [ $host == $db ]
+elif [$host == $db]
 then
 echo " db vm"
 
@@ -49,7 +49,8 @@ for d in $BDIR;do
 rsync $OPTS $d $BSERVER@$USERX:~/backup/db
 done
 #==========================================================================
-elif [ $host == $app ]
+elif [$host == $app]
+then
 then
 echo " app vm"
 
@@ -58,8 +59,10 @@ BDIR="
 /var/www/owncloud/data
 "
 
-sudo chmod o+rx /var/www/owncloud/data -R
- 
+# excludes file - this contains a wildcard pattern per line of files to exclude
+#EXCLUDES="$HOME/cron/excludes"
+#INCLUDE="$HOME/test/*.txt"
+# the name of the backup machine
 BSERVER=groupb
 USERX=restore-b.foo.org.nz
 BACKUPDIR=`date +%d-%m-%Y-%H-%M-%S`
@@ -68,8 +71,8 @@ OPTS="-haAXuv -v --exclude --log-file=/home/$USER/log.log --backup-dir=~/backup/
 for d in $BDIR;do
 rsync $OPTS $d $BSERVER@$USERX:~/backup/app
 done
-sudo chmod o-rx /var/www/owncloud/data -R
-else [ $host == $backup ]
+echo " app vm"
+else [$host == $backup]
 
 echo " backup vm"
 
