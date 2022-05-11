2e6fe59e9446adc2384e0e158d1886c625c70e0b
Anthony Legg <leggtc1@student.op.ac.nz>
2022-05-11 05:43:06 +0000

    mgmt: automated system backup
    completed: 11-05-2022 05:43:06 UTC
    
    mgmt-b

--- Mgmt-VM/puppet/code/modules/rsync/files/restore.sh
+++ Mgmt-VM/puppet/code/modules/rsync/files/restore.sh
@@ -38,7 +38,15 @@ elif [$host == $db]
 then
 echo " db vm"
 
-BDIR=""
+BDIR="
+/etc/node-exporter
+/etc/nagios
+/etc/nagios-plugins
+/etc/mysql
+/etc/puppet
+~/.bashrc
+~/.bash_history
+"
 # the name of the backup machine
 BSERVER=groupb
 USERX=restore-b.foo.org.nz
@@ -72,9 +80,31 @@ rsync $OPTS $d $BSERVER@$USERX:~/backup/app
 done
 
 sudo chmod o-rx /var/www/owncloud/data -R
-echo " app vm"
-else [ $host == $backup ]
 
+#====================================================
+else [ $host == $backup ]
+echo " db vm"
+
+BDIR="
+/etc/node-exporter
+/etc/nagios
+/etc/nagios-plugins
+/etc/mysql
+/etc/puppet
+~/.bashrc
+~/.bash_history
+/etc/prometheus
+/etc/prometheus-plugins
+"
+# the name of the backup machine
+BSERVER=groupb
+USERX=restore-b.foo.org.nz
+BACKUPDIR=`date +%d-%m-%Y-%H-%M-%S`
+OPTS="-haAXuv -v --exclude --log-file=/home/$USER/log.log --backup-dir=~/backup/backup/backup-$BACKUPDIR"
+
+for d in $BDIR;do
+rsync $OPTS $d $BSERVER@$USERX:~/backup/backup
+done
 echo " backup vm"
 
 fi
