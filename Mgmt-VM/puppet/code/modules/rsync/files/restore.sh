66c91d6df617a89e24865d5e1dfbca87ab258750
Anthony Legg <leggtc1@student.op.ac.nz>
2022-05-11 04:18:02 +0000

    mgmt: automated system backup
    completed: 11-05-2022 04:18:02 UTC
    
    mgmt-b

--- Mgmt-VM/puppet/code/modules/rsync/files/restore.sh
+++ Mgmt-VM/puppet/code/modules/rsync/files/restore.sh
@@ -33,12 +33,44 @@ rsync $OPTS $d $BSERVER@$USERX:~/backup/mgmt
 done
 
 
-
+#=========================================================================
 elif [$host == $db]
 then
 echo " db vm"
+
+BDIR=""
+# the name of the backup machine
+BSERVER=groupb
+USERX=restore-b.foo.org.nz
+BACKUPDIR=`date +%d-%m-%Y-%H-%M-%S`
+OPTS="-haAXuv -v --exclude --log-file=/home/$USER/log.log --backup-dir=~/backup/db/backup-$BACKUPDIR"
+
+for d in $BDIR;do
+rsync $OPTS $d $BSERVER@$USERX:~/backup/db
+done
+#==========================================================================
 elif [$host == $app]
 then
+then
+echo " app vm"
+
+BDIR="
+/etc/apache2
+/var/www/owncloud/data
+"
+
+# excludes file - this contains a wildcard pattern per line of files to exclude
+#EXCLUDES="$HOME/cron/excludes"
+#INCLUDE="$HOME/test/*.txt"
+# the name of the backup machine
+BSERVER=groupb
+USERX=restore-b.foo.org.nz
+BACKUPDIR=`date +%d-%m-%Y-%H-%M-%S`
+OPTS="-haAXuv -v --exclude --log-file=/home/$USER/log.log --backup-dir=~/backup/app/backup-$BACKUPDIR"
+
+for d in $BDIR;do
+rsync $OPTS $d $BSERVER@$USERX:~/backup/app
+done
 echo " app vm"
 else [$host == $backup]
 
