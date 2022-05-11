a8cd9a72548ab4bb0ac29c335e8687d23b6de096
Anthony Legg <leggtc1@student.op.ac.nz>
2022-05-11 08:00:03 +0000

    mgmt: automated system backup
    completed: 11-05-2022 08:00:03 UTC
    
    mgmt-b

--- Mgmt-VM/puppet/code/modules/rsync/files/restore.sh
+++ Mgmt-VM/puppet/code/modules/rsync/files/restore.sh
@@ -9,32 +9,13 @@ app=app-b.foo.org.nz
 host=$(hostname)
 if [ $host == $mgmt ]
 then
+SUFFIX=$(date +%j)
+remote_backup=restore-b.foo.org.nz
+#ssh groupb@$remote_backup ls Backups/my_work/backup_'$SUFFIX' && rm -r Backups/my_work/backup_'$SUFFIX'
 
-BDIR="
-/etc/node-exporter
-/etc/nagios
-/etc/nagios3
-/etc/nagios-plugins
-/etc/puppet/code
-/etc/puppet/puppet.conf
-"
+rsync -ab --recursive -v --files-from='to_backup.txt' --backup-dir=backup_$SUFFIX --delete --filter='protect backup_*' groupb$remote_backup:~/Backups/my_work/
 
-# excludes file - this contains a wildcard pattern per line of files to exclude
-EXCLUDES="$HOME/cron/excludes"
-INCLUDE="$HOME/test/*.txt"
-# the name of the backup machine
-BSERVER=groupb
-USERX=restore-b.foo.org.nz
-BACKUPDIR=`date +%d-%m-%Y-%H-%M-%S`
-OPTS="-haAXuv -v  --log-file=/home/$USER/log.log --backup-dir=~/backup/mgmt/backup-$BACKUPDIR" 
-
-for d in $BDIR;do
-rsync $OPTS $d $BSERVER@$USERX:~/backup/mgmt
-done
-
-
-#=========================================================================
-elif [ $host == $db ]
+elif [ $host == $db]	
 then
 echo " db vm"
 #~bash throw errer
