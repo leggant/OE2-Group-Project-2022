2d3cd27ea2dbc8c009ce2dee0861188686cf2c0d
Anthony Legg <leggtc1@student.op.ac.nz>
2022-05-15 20:32:00 +1200

    mgmt: removed unused txt files from rsync module

--- Mgmt-VM/puppet/code/modules/rsync/manifests/config.pp
+++ Mgmt-VM/puppet/code/modules/rsync/manifests/config.pp
@@ -1,28 +1,23 @@
 class rsync::config {
- 
-
-file { '/etc/backup':
-    ensure => 'directory',
-    owner  => 'root',
-    group  => 'bitstudent',
-    mode   => '0750',
+  file { '/etc/backup':
+      ensure => 'directory',
+      owner  => 'root',
+      group  => 'bitstudent',
+      mode   => '0750',
   }
  
-file { '/etc/backup/restore':
-        ensure => present,
-        source => 'puppet:///modules/rsync/restore.sh',
-        mode => '0775',
+  file { '/etc/backup/restore':
+      ensure => present,
+      source => 'puppet:///modules/rsync/restore.sh',
+      mode => '0775',
 	}
 
-file { "backup.cron":
-    path    => "/etc/cron.hourly/backup.cron",
-    ensure  => present,
-    owner   => "root",
-    group   => "root",
-    mode    => "0775",
-    content => "* 2 * * * root /etc/backup/restore"
+  file { "backup.cron":
+      path    => "/etc/cron.hourly/backup.cron",
+      ensure  => present,
+      owner   => "root",
+      group   => "root",
+      mode    => "0775",
+      content => "* 2 * * * root /etc/backup/restore"
+  }
 }
-
-}
-
-
