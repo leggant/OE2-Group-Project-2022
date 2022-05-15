bfa4f287d2ca631ce5a11bce676fbacba3d9a4dd
Anthony Legg <leggtc1@student.op.ac.nz>
2022-05-15 20:53:46 +0000

    mgmt: automated system backup
    completed: 15-05-2022 20:53:46 UTC
    
    mgmt-b

--- Mgmt-VM/puppet/code/modules/rsync/files/restore.sh
+++ Mgmt-VM/puppet/code/modules/rsync/files/restore.sh
@@ -16,8 +16,6 @@ BDIR="
 /etc/puppet/code
 /etc/puppet/puppet.conf
 "
-EXCLUDES="$HOME/cron/excludes"
-INCLUDE="$HOME/test/*.txt"
 # the name of the backup machine
 BSERVER=groupb
 USERX=restore-b.foo.org.nz
@@ -28,7 +26,7 @@ for d in $BDIR;do
 rsync $OPTS $d $BSERVER@$USERX:~/backup/mgmt
 done
 #=====================================================================================================
-elif [ $host == $db ]	
+elif [ $host == $db ]
 then
 echo " db vm"
 #~bash throw errer
@@ -48,7 +46,7 @@ USERX=restore-b.foo.org.nz
 BACKUPDIR=`date +%d-%m-%Y-%H-%M-%S`
 OPTS="-haAXuv -v --exclude --log-file=/home/$USER/log.log --backup-dir=~/backup/db/backup-$BACKUPDIR"
 
-## MYSQL BACKUP
+# MYSQL BACKUP
 SQLDATE=`date +%d%m%Y%H%M%S`
 sudo mysqldump --skip-extended-insert --all-databases --add-drop-table > "~/mysql-backup/$SQLDATE.sql"
 sudo chown -R bitstudent ~/mysql-backup
@@ -59,8 +57,6 @@ for d in $BDIR;do
 rsync $OPTS $d $BSERVER@$USERX:~/backup/db
 done
 sudo chmod o-rx /etc/mysql/debian.cnf
-unset SQLDATE
-unset BACKUPDIR
 #==========================================================================
 elif [ $host == $app ]
 then
@@ -72,8 +68,6 @@ BDIR="
 "
 sudo chmod o+rx /var/www/owncloud/data -R
 # excludes file - this contains a wildcard pattern per line of files to exclude
-#EXCLUDES="$HOME/cron/excludes"
-#INCLUDE="$HOME/test/*.txt"
 # the name of the backup machine
 BSERVER=groupb
 USERX=restore-b.foo.org.nz
