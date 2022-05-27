b975d449c0dec9ef896f9298c4f6e0951c097523
Anthony Legg <leggtc1@student.op.ac.nz>
2022-05-27 11:30:03 +0000

    mgmt: automated system backup
    completed: 27-05-2022 11:30:03 UTC
    
    mgmt-b

--- Mgmt-VM/puppet/code/modules/rsync/files/restore.sh
+++ Mgmt-VM/puppet/code/modules/rsync/files/restore.sh
@@ -46,11 +46,11 @@ sudo chmod o+rx /etc/mysql/debian.cnf
 
 # MYSQL BACKUP
 MYSQLDATE=`date +%d%m%Y%H%M%S`
-cd ~/mysql-backup
-sudo mysqldump --skip-extended-insert --all-databases --add-drop-table > sqlbackup.sql
-sudo chown -R bitstudent ~/mysql-backup
-sudo chgrp -R bitstudent ~/mysql-backup
-sudo chmod 770 -R ~/mysql-backup/
+cd /etc/backup
+sudo mysqldump  --skip-extended-insert --all-databases --add-drop-table > sqlbackup.sql
+sudo chown  bitstudent /etc/backup/sqlbackup.sql
+sudo chgrp  bitstudent /etc/backup/sqlbackup.sql 
+sudo chmod 770 /etc/backup/sqlbackup.sql
 
 # the name of the backup machine
 BSERVER=groupb
